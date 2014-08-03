0009d8d63ca0d81e74b5f750b761f345991df0e5
Nero Sule <sulenero@gmail.com>
2014-08-03 15:58:12 +0100

    Fixes date validation.

--- Tracker/App_Start/BundleConfig.cs
+++ Tracker/App_Start/BundleConfig.cs
@@ -10,10 +10,18 @@ namespace Tracker
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery")
                         .Include("~/Scripts/jquery-{version}.js")
-                        .Include("~/Scripts/jquery.unobtrusive-ajax.js"));
+                        .Include("~/Scripts/jquery.unobtrusive-ajax.js")
+                        .Include("~/Scripts/globalize/globalize.js")
+                        .Include("~/Scripts/globalize/cultures/globalize.culture.en-GB.js")
+                        
+                        );
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval")
+                .Include(
+                "~/Scripts/jquery.validate.js", 
+                "~/Scripts/jquery.validate.unobtrusive.js")
+                .Include("~/Scripts/jquery.validate.globalize.js")
+                );
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -23,7 +31,9 @@ namespace Tracker
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js",
-                      "~/Scripts/bootstrap-datepicker.js"));
+                      "~/Scripts/bootstrap-datepicker.js",
+                      "~/Scripts/bootstrap-datepicker-globalize.js")
+                      );
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-slate.css",
